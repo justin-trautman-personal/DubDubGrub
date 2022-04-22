6414c35528662e1f1c2eb2240aa451f91aa8d993
Justin <justin@digitalawesomeapps.com>
2022-04-22 13:37:18 -0700

    Initial Commit

---
+++ DubDubGrub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DubDubGrub
+//
+//  Created by Justin Trautman on 4/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Justin Trautman on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
